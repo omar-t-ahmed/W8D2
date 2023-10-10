bab7384e250da2103043e832064c8345118cf138
omar-t-ahmed <omartahmed@gmail.com>
2023-10-10 17:03:24 -0400

    finish p1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -76,25 +76,50 @@ Array.prototype.myEach = function(callback) {
 
     Array.prototype.myMap = function(callback){
         let newArr = [];
-        for (let i = 0; i < this.length; i++){
-            newArr.push(callback(this[i]));
-        }
+        // for (let i = 0; i < this.length; i++){
+        //     newArr.push(callback(this[i]));
+        // }
+        this.myEach(function(ele) {
+            newArr.push(callback(ele))
+        })
+        // this.myEach(ele => newArr.push(callback(ele)))
+
         return newArr;
     }
 
 
-
-
-
 // console.log([1, 2, 3, 4, 5].myEach(callback));
-console.log([1, 2, 3, 4, 5].myMap(doubler));
+// console.log([1, 2, 3, 4, 5].myMap(doubler));
 
 
+Array.prototype.myReduce = function(callback, initialValue) {
+    let acc;
+    let arr;
+    if (initialValue !== undefined) {
+        acc = initialValue;
+        arr = this;
+    } else {
+        acc = this[0];
+        arr = this.slice(1);
+    }
+    
+    arr.myEach(function(ele) {
+        acc = callback(acc, ele)
+    }) 
 
+    // arr.myEach(callback)
 
+    return acc
+}
 
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  })); // => 6
 
-
+  // with initialValue
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+    }, 25)); // => 31
 
 
 
