de2a1a34b9d2bc1e7aac4d1e63bc25769b6091d0
“Eltion <ebehrami01@gmail.com>
2023-10-10 14:43:14 -0400

    working on first problem

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,36 @@
 // Your code here
+
+Array.prototype.uniq = function() {
+
+    let obj = {};
+
+    // this.forEach((ele) => {
+    //     if (obj[ele] === null) {
+    //     obj[ele] = 0 
+    //     }
+    //     else 
+    //     {
+    //     obj[ele] += 1
+    //     }
+    // }
+    // )
+
+    
+    
+    for (let i = 0; i < this.length; i++){
+        let ele = this[i];
+        if (obj[ele] === undefined) {
+            obj[ele] = 1;
+        } 
+    }
+    return obj.keys;
+}
+
+const newArray = [1,2,2,3,3,3]
+
+console.log(newArray.uniq());
+
+// console.log(Array.prototype.uniq);
+
+
+
