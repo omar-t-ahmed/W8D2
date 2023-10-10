7a7c10388ad8410c97b9c3c6f02fa534941d2507
omar-t-ahmed <omartahmed@gmail.com>
2023-10-10 15:35:34 -0400

    phase1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,36 +1,65 @@
 // Your code here
 
 Array.prototype.uniq = function() {
-
-    let obj = {};
-
-    // this.forEach((ele) => {
-    //     if (obj[ele] === null) {
-    //     obj[ele] = 0 
-    //     }
-    //     else 
-    //     {
-    //     obj[ele] += 1
-    //     }
+    // let obj = {};
+    // for (let i = 0; i < this.length; i++){
+    //     let ele = this[i];
+    //     if (obj[ele] === undefined) {
+    //         obj[ele] = 1;
+    //     } 
     // }
-    // )
-
-    
-    
-    for (let i = 0; i < this.length; i++){
-        let ele = this[i];
-        if (obj[ele] === undefined) {
-            obj[ele] = 1;
-        } 
+    // return Object.keys(obj);
+    let newArr = []
+    for (let i  = 0; i < this.length; i++) {
+        if(!newArr.includes(this[i])) {
+            newArr.push(this[i])
+        }
     }
-    return obj.keys;
+    return newArr
+}
+// console.log([1,2,2,3,3,3].uniq());
+
+Array.prototype.twoSum = function() {
+    let newArr = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) {
+                newArr.push([i, j])
+            }
+        }
+    }
+    return newArr
 }
 
-const newArray = [1,2,2,3,3,3]
+// console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
 
-console.log(newArray.uniq());
+Array.prototype.transpose = function() {
+    // let newArr = []
+    // for (let i = 0; i < this.length; i++) {
+    //     let subArr = this[i]
+    //     for (let j = 0; j < subArr.length; j++ ) {
+    //         newArr.push(subArr[j])
+    //     }
+    // }
+    // return newArr
 
-// console.log(Array.prototype.uniq);
+    let newArr = [[],[],[]]
+    // i = 0
+    // while (i < this.length) {
+    //     newArr.push([])
+    //     i += 1
+    // }
+    
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++ ) {
+            newArr.push([j][i])
+        }
+    }
+    return newArr
+}
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose()) // => [[0,3,6], [1,4,7], [2,5,8]]
+
 
 
 
